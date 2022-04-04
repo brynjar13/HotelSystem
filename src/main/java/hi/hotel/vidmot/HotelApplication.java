fb0eb31b8a8e9c0c09a10d9d7c495f84a9456c90
brynjar13 <binnibja@gmail.com>
2022-04-04 17:17:53 +0000

    first commit

---
+++ src/main/java/hi/hotel/vidmot/HotelApplication.java
@@ -0,0 +1,23 @@
+package hi.hotel.vidmot;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HotelApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HotelApplication.class.getResource("frontpage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
+        stage.setTitle("Hotels");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/hi/hotel/vidmot/HotelController.java
@@ -0,0 +1,8 @@
+package hi.hotel.vidmot;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HotelController {
+
+}
