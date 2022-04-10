8bf99e9c2fc279325ac7adf1d3bf7dd0d0afdf78
brynjar13 <binnibja@gmail.com>
2022-04-10 12:12:29 +0000

    skipta um scene

--- src/main/java/hi/hotel/vidmot/HotelController.java
+++ src/main/java/hi/hotel/vidmot/HotelController.java
@@ -3,8 +3,7 @@ package hi.hotel.vidmot;
 import hi.hotel.database.BookingFile;
 import hi.hotel.database.HotelFile;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.*;
 import hi.hotel.vinnsla.Bokun;
 import hi.hotel.vinnsla.Herbergi;
 import hi.hotel.vinnsla.Hotel;
@@ -15,11 +14,14 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.skin.LabeledSkinBase;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -27,6 +29,8 @@ import java.util.ResourceBundle;
 public class HotelController implements Initializable {
     HotelFile hotelFile;
     ArrayList<Hotel> hotels;
+    private String hotelInfoName;
+    private int hotelInfoId;
 
     @FXML
     private TextField textInput;
@@ -57,5 +61,30 @@ public class HotelController implements Initializable {
         }
         ObservableList<Hotel> oHotelList = FXCollections.observableArrayList(tempHotelList);
         hotelList.setItems(oHotelList);
+    }
+
+    public void clickHotel(MouseEvent e) throws IOException {
+        if (!e.getTarget().getClass().getName().toString().equals("com.sun.javafx.scene.control.LabeledText")) {
+            e.consume();
+            return;
+        }
+        Text hotel = (Text) e.getTarget();
+        for (Hotel rightHotel:
+             hotels) {
+            if (rightHotel.getName().equals(hotel.getText())) {
+                hotelInfoName = rightHotel.getName();
+                hotelInfoId = rightHotel.getId();
+            }
+        }
+        FXMLLoader fxmlLoader = new FXMLLoader(HotelApplication.class.getResource("hotel.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
+        HotelViewController hvc =  fxmlLoader.getController();
+        hvc.setHotelName(hotelInfoName);
+        hvc.setHotelId(hotelInfoId);
+        Stage stage = (Stage) hotelList.getScene().getWindow();
+        scene.setUserData(fxmlLoader.getController());
+        stage.setTitle("Hotel");
+        stage.setScene(scene);
+        stage.show();
     }
 }
