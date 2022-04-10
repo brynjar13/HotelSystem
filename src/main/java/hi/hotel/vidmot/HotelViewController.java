5ed09abaa57793629881aa1d2a434243411bf458
brynjar13 <binnibja@gmail.com>
2022-04-10 15:44:31 +0000

    bookingform

--- src/main/java/hi/hotel/vidmot/HotelViewController.java
+++ src/main/java/hi/hotel/vidmot/HotelViewController.java
@@ -5,7 +5,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,11 +20,25 @@ import java.util.ResourceBundle;
 public class HotelViewController implements Initializable {
     @FXML
     private Label fxHotelName;
+    @FXML
+    private ImageView fxHotelImage;
+    @FXML
+    private DatePicker fxCheckinDate;
+    @FXML
+    private DatePicker fxCheckoutDate;
+    @FXML
+    private TextField fxName;
+    @FXML
+    private TextField fxEmail;
+    @FXML
+    private  TextField fxKennitala;
+    @FXML
+    private Button fxBokun;
     private int hotelId;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        fxBokun.disableProperty().bind(fxCheckinDate.valueProperty().isNull().or(fxCheckoutDate.valueProperty().isNull().or(fxName.textProperty().isEmpty().or(fxEmail.textProperty().isEmpty().or(fxKennitala.textProperty().isEmpty())))));
     }
 
     public void setHotelName(String name) {
@@ -28,6 +47,10 @@ public class HotelViewController implements Initializable {
 
     public void setHotelId(int id) {
         hotelId = id;
+        if (hotelId == 1) {
+            Image hotelImage = new Image(HotelViewController.class.getResourceAsStream("myndir/francesca-saraco-_dS27XGgRyQ-unsplash.jpg"));
+            fxHotelImage.setImage(hotelImage);
+        }
     }
 
     @FXML
